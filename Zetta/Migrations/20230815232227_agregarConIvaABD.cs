86de4b05bd5c903797c33045e09b262cf0ff2676
Francososa <franco-sosa@hotmail.com>
2023-08-15 21:02:30 -0300

    Agregue Tabla "CondIva" y tabla "Provincia"

---
+++ Zetta/Migrations/20230815232227_agregarConIvaABD.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Zetta.Migrations
+{
+    public partial class agregarConIvaABD : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CondIva",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CondIva", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CondIva");
+        }
+    }
+}
---
+++ Zetta/Models/Provincia.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Zetta.Models
+{
+    public class Provincia
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Nombre { get; set; }
+    }
+}
