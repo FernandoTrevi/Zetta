104de5f5d52cc25ad3e923182b5bd4b2aed64d7b
Fernando Trevi <trevi83.ft@gmail.com>
2023-08-30 14:35:31 -0300

    Agregué Resúmen de compra del usuario: (UsuarioAplicaciónVM y Vista Resúmen)

--- Zetta/Controllers/CarroController.cs
+++ Zetta/Controllers/CarroController.cs
@@ -1,14 +1,19 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.Security.Claims;
 using Zetta.Datos;
 using Zetta.Models;
+using Zetta.Models.ViewModels;
 using Zetta.Utilidades;
 
 namespace Zetta.Controllers
 {
     public class CarroController : Controller
     {
+        [BindProperty]
+        public ProductoUsuarioVM productoUsuarioVM { get; set; }
         private readonly ApplicationDbContext _db;
         public CarroController(ApplicationDbContext db)
         {
@@ -25,6 +30,46 @@ namespace Zetta.Controllers
             List <int> prodEnCarro = carroComprasList.Select(i=> i.ProductoId).ToList();
             IEnumerable<Producto> productoList = _db.Producto.Where(p=>prodEnCarro.Contains(p.Id)).ToList();
             return View(productoList);
+        }
+
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Index")]
+        public IActionResult IndexPost()
+        {
+            return RedirectToAction(nameof(Resumen));
+        }
+
+        public IActionResult Resumen() 
+        {
+            //Trae al usuario conectado
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claims = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            //
+
+            //Obtiene la lista de productos de la sesión
+            List<CarroCompra> carroComprasList = new List<CarroCompra>();
+            if (HttpContext.Session.Get<IEnumerable<CarroCompra>>(WC.SessionCarroCompras) != null &&
+               HttpContext.Session.Get<IEnumerable<CarroCompra>>(WC.SessionCarroCompras).Count() > 0)
+            {
+                carroComprasList = HttpContext.Session.Get<List<CarroCompra>>(WC.SessionCarroCompras);
+            }
+            List<int> prodEnCarro = carroComprasList.Select(i => i.ProductoId).ToList();
+            IEnumerable<Producto> productoList = _db.Producto.Where(p => prodEnCarro.Contains(p.Id)).ToList();
+            //
+
+            //Llena el viewModels
+            productoUsuarioVM = new ProductoUsuarioVM()
+            {
+                UsuarioAplicacion = _db.UsuarioAplicacion.FirstOrDefault(u => u.Id == claims.Value),
+                ProductoLista = productoList,
+
+            };
+            //
+
+            //Retorna el ViewModels a la vista Resúmen
+            return View(productoUsuarioVM); 
         }
 
         public IActionResult Remover(int Id)
